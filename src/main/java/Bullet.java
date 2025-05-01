c046b351dff25aafed41219868cbfacb6e03b897
simpl3man <157831131+coderms1@users.noreply.github.com>
2025-05-01 11:01:37 -0400

    v2 update

--- src/main/java/Bullet.java
+++ src/main/java/Bullet.java
@@ -1,36 +1,33 @@
 import java.awt.*;
 
-// REPRESENTS A BULLET FIRED BY PLAYER OR ENEMY
 public class Bullet {
-    private int x, y;  // BULLET POSITION
-    private boolean isPlayer; // INDICATES IF BULLET IS PLAYER-FIRED
+    private int x, y; // POSITION
+    private boolean isPlayer; // DETERMINES 'IS PLAYER-FIRED' or FALSE
 
-    // INITIALIZES BULLET POSITION AND TYPE
+    // BULLET POSITION & TYPE
     public Bullet(int x, int y, boolean isPlayer) {
         this.x = x;
         this.y = y;
         this.isPlayer = isPlayer;
     }
 
-    // UPDATES BULLET POSITION BASED ON TYPE (IS PLAYER? OR NOT)
+    // UPDATES BULLET (BASED on TYPE --> 'IS PLAYER?' or NOT)
     void move() {
         y += isPlayer ? -10 : 5;
     }
 
-    // RETURNS A COLLISION RECTANGLE FOR THE BULLET
+    // COLLISION RECTANGLE
     Rectangle getBounds() {
-        return new Rectangle(x, y, 5, 10);
+        return new Rectangle(x, y, 5, 9); // REDUCED 50% FROM 9X18
     }
 
     // GETTERS
     public int getX() {
         return x;
     }
-
     public int getY() {
         return y;
     }
-
     public boolean isPlayer() {
         return isPlayer;
     }
@@ -39,16 +36,14 @@ public class Bullet {
     public void setX(int x) {
         this.x = x;
     }
-
     public void setY(int y) {
         this.y = y;
     }
-
     public void setPlayer(boolean isPlayer) {
         this.isPlayer = isPlayer;
     }
 
-    // RETURNS A STRING REPRESENTATION OF THE BULLET
+    // toString() - RETURNS STRING of BULLET
     @Override
     public String toString() {
         return "Bullet [x=" + x + ", y=" + y + ", isPlayer=" + isPlayer + "]";
